241c3f8da66c3fcb46f4445778e2d1f358a18100
Ajay Verma <verajay426@gmail.com>
2025-08-27 08:31:47 +0530

    Initial commit - Node.js Hello World API

---
+++ Day4.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
